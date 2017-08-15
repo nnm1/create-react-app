0caa004481cbd2b132968534aac974e6d3f23935
skanukov <s.kanukov@gmail.com>
2017-08-15 18:32:31 +0300

    Compile SCSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './assets/styles/index.css';
+import './assets/styles/app.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
