8b7a85a7acfe89cb4115a1311c5d37f1a7422c66
skanukov <s.kanukov@gmail.com>
2017-08-15 18:44:39 +0300

    Do not use service workers

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 
 import './assets/styles/app.css';
 import App from './components/App';
-import registerServiceWorker from './lib/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
