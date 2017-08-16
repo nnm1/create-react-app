9f7b2793877f1e9d6e69efcfa2072aa075131157
skanukov <s.kanukov@gmail.com>
2017-08-16 11:15:04 +0300

    Render SideMenu

--- src/components/ui/layout/sidebar/SideMenu.jsx
+++ src/components/ui/layout/sidebar/SideMenu.jsx
@@ -1,15 +1,67 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-import deviceUtil from '../../../../lib/utils/deviceUtil'
+const menu = {
+  main: [
+    { label: 'Главная', to: '/' }
+  ],
 
-class SideMenu extends React.Component {
-  render() {
+  footer: [
+    {
+      label: 'Настройки',
+      nested: [
+        {
+          label: 'Профиль',
+          to: '/settings/profile'
+        }
+      ]
+    },
+    { label: 'Выйти', to: '/auth/logout' }
+  ]
+}
+
+const renderLink = link => {
+  const renderNavLink = () => {
     return (
-      <nav className="sidemenu">
-        
-      </nav>
+      <li className="nav-item" key={link.to}>
+        <NavLink exact to={link.to} className="nav-link" activeClassName="active"
+          onClick={this._handleNavLinkClick}>
+          {link.label}
+        </NavLink>
+        {link.nested && renderMenu(link.nested, 'nested')}
+      </li>
     )
   }
+
+  const renderLabelLink = () => {
+    return (
+      <li className="nav-item" key={link.label}>
+        <a className="nav-link">
+          {link.label}
+        </a>
+        {link.nested && renderMenu(link.nested, 'nested')}
+      </li>
+    )
+  }
+
+  return link.to ? renderNavLink() : renderLabelLink()
+}
+
+const renderMenu = (menuLinks, additionalClassName = '') => {
+  return (
+    <ul className={'nav flex-column ' + additionalClassName}>
+      {menuLinks.map(link => renderLink(link))}
+    </ul>
+  )
+}
+
+const SideMenu = () => {
+  return (
+    <nav className="sidemenu">
+      {renderMenu(menu.main)}
+      {renderMenu(menu.footer, '-sidemenu-footer')}
+    </nav>
+  )
 }
 
 export default SideMenu
