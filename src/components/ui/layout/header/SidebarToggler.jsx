d73b2eb1695d9e118e43f48dc12617e53e67bb19
skanukov <s.kanukov@gmail.com>
2017-08-16 11:29:17 +0300

    Refactor SidebarToggler

--- src/components/ui/layout/header/SidebarToggler.jsx
+++ src/components/ui/layout/header/SidebarToggler.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
 
-class SidebarToggler extends React.Component {
-  constructor(props) {
-    super(props)
+const handleSidebarToggle = () => {
+  document.dispatchEvent(new Event('onSidebarToggle'))
+}
 
-    this._handleSidebarToggle = this._handleSidebarToggle.bind(this)
-  }
-
-  _handleSidebarToggle() {
-    document.dispatchEvent(new Event('onSidebarToggle'))
-  }
-
-  render() {
-    return (
-      <button className="app-sidebar-toggler navbar-toggler d-md-none" type="button"
-        aria-label="Toggle navigation" onClick={this._handleSidebarToggle}>
-        <span className="navbar-toggler-icon" />
-      </button>
-    )
-  }
+const SidebarToggler = () => {
+  return (
+    <button className="app-sidebar-toggler navbar-toggler d-md-none" type="button"
+      aria-label="Toggle navigation" onClick={handleSidebarToggle}>
+      <span className="navbar-toggler-icon" />
+    </button>
+  )
 }
 
 export default SidebarToggler
