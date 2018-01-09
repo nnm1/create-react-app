9176548639491be155daf90598ae48288fb4c51b
skanukov <s.kanukov@gmail.com>
2018-01-09 14:53:42 +0300

    Reimplement a content layout

--- src/components/layout/MyContent.jsx
+++ src/components/layout/MyContent.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const MyContent = ({ header, actions, content }) => (
-  <div className="container-fluid">
-    <div className="my-2 header">
+  <React.Fragment>
+    <header className="container-fluid pt-2 header">
       <div className="row align-items-center">
         <div className="col">{header}</div>
         {actions && <div className="col-auto action-group">{actions}</div>}
       </div>
-    </div>
+    </header>
 
-    <main className="content">{content}</main>
-  </div>
+    <main className="container-fluid py-2 content">{content}</main>
+  </React.Fragment>
 )
 
 export default MyContent
