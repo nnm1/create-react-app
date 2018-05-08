5f8aed0975c384ba86f86aab1cb685e4fad14d78
skanukov <s.kanukov@gmail.com>
2018-05-08 10:15:22 +0300

    Update deps

--- src/components/layout/MyContent.jsx
+++ src/components/layout/MyContent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function MyContent({ header, actions, content }) {
   return (
-    <div className="app-content">
+    <div className="my-content">
       <header className="container-fluid py-2 header">
         <div className="row align-items-center">
           <div className="col-12 col-sm">{header}</div>
