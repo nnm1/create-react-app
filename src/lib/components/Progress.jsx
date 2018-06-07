46e39335c639668e3cf8524b479cc776d0c2d892
skanukov <s.kanukov@gmail.com>
2018-06-07 10:36:44 +0300

    Use Bootstrap progress

--- src/lib/components/Progress.jsx
+++ src/lib/components/Progress.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Progress() {
   return (
-    <div className="text-center">
-      <progress />
+    <div className="progress mx-auto" style={{ width: '150px' }}>
+      <div
+        className="progress-bar progress-bar-striped progress-bar-animated"
+        style={{ width: '100%' }}
+        role="progressbar"
+      />
     </div>
   )
 }
